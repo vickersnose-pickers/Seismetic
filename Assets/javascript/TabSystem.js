83c760d7fc6c3543c81638fd6a702a5e8643ad33
rxfe-a <28RafaelA539@gmail.com>
2024-04-07 21:33:46 -0700

    functionality 1

--- Assets/javascript/TabSystem.js
+++ Assets/javascript/TabSystem.js
@@ -1,8 +1,14 @@
 let TabCount = 0;
-
+let SelectedTab = 0
 function CreateTab() {
     TabCount = TabCount + 1
     const Identifier = TabCount;
+    SelectedTab = Identifier;
+    HideIframes();
+    const Iframe = document.createElement('iframe');
+    document.getElementById('Iframeholder').appendChild(Iframe);
+    Iframe.src = `/BrowserPages/`;
+    Iframe.id = `Tab${Identifier}_IFRAME`;
     const TabDiv = document.createElement('div');
     TabDiv.className = 'Tab';
     TabDiv.id = 'Tab' + Identifier;
@@ -15,10 +21,51 @@ function CreateTab() {
       document.getElementById('BrowserAppend').appendChild(TabDiv);
       document.head.appendChild(document.getElementById('add'))
       document.getElementById('BrowserAppend').appendChild(document.getElementById('add'))
+      TabDiv.onclick = function () {
+        SelectedTab = Identifier
+        HideIframes()
+        Iframe.style.display = 'flex';
+      }
 }
 
 function KillTab(id) {
     document.getElementById('Tab' + id).remove()
+    document.getElementById(`Tab${id}_IFRAME`).remove()
 }
 
 CreateTab()
+
+
+function HideIframes() {
+  var iframeHolder = document.getElementById('Iframeholder');
+    if (iframeHolder) {
+        var iframes = iframeHolder.getElementsByTagName('iframe');
+        for (var i = 0; i < iframes.length; i++) {
+            iframes[i].style.display = 'none';
+        }
+    } else {
+        console.error("Element with ID 'IframeHolder' not found.");
+    }
+}
+
+/////////////
+
+document.getElementById('back').onclick = function () {
+  const Iframe = document.getElementById(`Tab${SelectedTab}_IFRAME`);
+  Iframe.contentWindow.history.back();
+}
+
+document.getElementById('forward').onclick = function () {
+  const Iframe = document.getElementById(`Tab${SelectedTab}_IFRAME`);
+  Iframe.contentWindow.history.forward();
+}
+
+document.getElementById('refresh').onclick = function () {
+  const Iframe = document.getElementById(`Tab${SelectedTab}_IFRAME`);
+  Iframe.contentDocument.location.reload();
+}
+
+document.getElementById('home').onclick = function () {
+  const Iframe = document.getElementById(`Tab${SelectedTab}_IFRAME`);
+  Iframe.src = '/BrowserPages/';
+}
--- Assets/javascript/loader.js
+++ Assets/javascript/loader.js
@@ -13,44 +13,27 @@ function startMain() {
   
   if (checkPopupPermissions()) {
     if (window.self !== window.top) { console.log('Iframed'); } else { const url = window.location.href + 'browser/'; const ScriptURL = window.location.href + 'Assets/javascript/TabSystem.js'; const CSS_URL = window.location.href + 'Assets/css/browser.css'; var newWindow = window.open("", "newWindow", "width=1200,height=750"); newWindow.document.write( `
-    <!DOCTYPE html>
-
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="${CSS_URL}">
-<link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..30,100..700,0..1,-50..100" />
-</head>
-
-<body>
-  <div class="BrowserBar" id="BrowserAppend">
-    <button class="adds" id="add" onclick="CreateTab()">+</button>
-  </div>
-  <div class="ActionBar">
-    <div class="BrowserControls">
-      <button id="back"><span class="material-symbols-outlined">arrow_back</span></button>
-      <button id="forward"><span class="material-symbols-outlined">arrow_forward</span></button>
-      <button id="refresh"><span class="material-symbols-outlined">refresh</span></button>
-      <button id="home"><span class="material-symbols-outlined">home</span></button>
-    </div>
-    <div class="BrowserSearch">
-      <div class="SafetyIcon"><span class="material-symbols-outlined">
-        lock
-        </span></div>
-      <input placeholder="Search or type a URL" id="BrowserBar">
-    </div>
-    <div class="BrowserExtras">
-      <button id="forward"><span class="material-symbols-outlined">extension</span></button>
-      <button id="refresh"><span class="material-symbols-outlined">dock_to_left</span></button>
-      <button id="home"><span class="material-symbols-outlined">tune</span></button>
-    </div>
-  </div>
-  <div id="Iframeholder">
-  </div>
-  <script src="${ScriptURL}"></script>
-</body>
-</html>` ); window.location.href = 'https://google.com'; }
+    <html>
+  <head>
+    <style>
+      body {
+        margin: 0;
+        padding: 0;
+        height: 100vh;
+      }
+      
+      iframe {
+        width: 100%;
+        height: 100vh;
+        border: none;
+      }
+    </style>
+  </head>
+  <body>
+    <iframe src='${url}'>
+  </body>
+ </html>
+    ` ); window.location.href = 'https://google.com'; }
   } else {
     document.getElementById('Popup').style.display = 'flex';
   }
