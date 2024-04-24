880019c231bb475bd004a3c86cedea4fb8f110b7
rxfe-a <28RafaelA539@gmail.com>
2024-04-23 18:52:20 -0700

    mom

---
+++ src/loaders/rh.js
@@ -0,0 +1,86 @@
+import express from 'express';
+import http from 'node:http';
+import { createBareServer } from "bsn";
+import { dynamicPath } from "@nebula-services/dynamic";
+import path from 'node:path';
+import createRammerhead from "rammerhead/src/server/index.js";
+
+const PORT = 4242;
+const BareDirectory = '/bare/';
+const FrontEnd = 'public';
+const BrowserPages = 'BrowserPages';
+const __dirname = process.cwd();
+const rh = createRammerhead();
+
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer(BareDirectory);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, FrontEnd)));
+app.use('/BrowserPages', express.static(path.join(__dirname, `/src/${BrowserPages}`)));
+app.use("/dynamic/", express.static(dynamicPath));
+app.use('/Assets', express.static(path.join(__dirname, '/src/assets')));
+
+const rammerheadScopes = [
+	"/rammerhead.js",
+	"/hammerhead.js",
+	"/transport-worker.js",
+	"/task.js",
+	"/iframe-task.js",
+	"/worker-hammerhead.js",
+	"/messaging",
+	"/sessionexists",
+	"/deletesession",
+	"/newsession",
+	"/editsession",
+	"/needpassword",
+	"/syncLocalStorage",
+	"/api/shuffleDict"
+];
+const rammerheadSession = /^\/[a-z0-9]{32}/;
+function shouldRouteRh(req) {
+	const url = new URL(req.url, "http://0.0.0.0");
+	return (
+	  rammerheadScopes.includes(url.pathname) ||
+	  rammerheadSession.test(url.pathname)
+	);
+  }
+  
+  function routeRhRequest(req, res) {
+	rh.emit("request", req, res);
+  }
+
+  function routeRhUpgrade(req, socket, head) {
+	rh.emit("upgrade", req, socket, head);
+  }
+
+app.use((req, res, next) => {
+  if(shouldRouteRh(req)) rh.emit("request", req, res); else next();
+});
+
+server.on('request', (req, res) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeRequest(req, res);
+      } else if (shouldRouteRh(req)) {
+        routeRhRequest(req, res);
+      } else {
+        app(req, res);
+      }
+});
+
+server.on('upgrade', (req, socket, head) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeUpgrade(req, socket, head);
+      } else if (shouldRouteRh(req)) {
+        routeRhUpgrade(req, socket, head);
+      } else {
+		socket.end();
+	  }
+});
+
+server.listen(PORT, () => {
+    console.log(`✅ | Successfully started Seismetic`);
+    console.log(`👁️ | Running at http://localhost:${PORT}`);
+});
--- src/test-service.mjs
+++ src/test-service.mjs
@@ -28,11 +28,95 @@ function help() {
     console.log();
     console.log('npm test parrot / parrot');
     console.log();
+    console.log('npm test version / Shows the Seismetic Version');
+    console.log();
+    console.log('npm test get_rh_sessions / Gets all the Rammerhead Sessions');
+    console.log();
+    console.log('npm test remove_rh_session <Param> / Removes the Rammerhead Session ');
+    console.log();
 }
 
 function parrot() {
     exec('curl parrot.live')
 }
 
+function install() {
+    const installer = process.argv[3];
+    eval(`${installer}_install`)
+}
+
+
+async function resetboot() {
+    try {
+      const data = await readFile('./src/loaders/default.js', 'utf8');
+      console.log('// Reseting Bootloader')
+      await writeFile('./src/index.mjs', data);
+      console.log('Data written to index.mjs successfully!');
+      const config = {
+        isRammerheadEnabled: false,
+        isNodeUnblockerEnabled: false
+      };
+      const jsonConfig = JSON.stringify(config, null, 2);
+      fs.writeFileSync('./public/config.json', jsonConfig, 'utf8');
+    } catch (error) {
+      console.error('Error reading or writing file:', error);
+    }
+  }
+
+  async function rammerhead_install() {
+    exec('npm install https://github.com/holy-unblocker/rammerhead/releases/download/v1.2.41-holy.5/rammerhead-1.2.41-holy.5.tgz')
+    try {
+      const data = await readFile('./src/loaders/rh.js', 'utf8');
+      console.log('// Reseting Bootloader')
+      await writeFile('./src/index.mjs', data);
+      console.log('Data written to index.mjs successfully!');
+      const config = {
+        isRammerheadEnabled: false,
+        isNodeUnblockerEnabled: false
+      };
+      const jsonConfig = JSON.stringify(config, null, 2);
+      fs.writeFileSync('./public/config.json', jsonConfig, 'utf8');
+    } catch (error) {
+      console.error('Error reading or writing file:', error);
+    }
+  }
+
+function get_rh_sessions() {
+    const sessionsDir = './node_modules/rammerhead/sessions';
+      fs.readdir(sessionsDir, (err, files) => {
+          if (err) {
+              console.error('Error reading directory:', err);
+              return;
+          }
+  
+          const fileNames = files.filter(file => fs.statSync(path.join(sessionsDir, file)).isFile());
+          console.log('IDs Detected:');
+          fileNames.forEach(fileName => console.log(fileName));
+      });
+  }
+
+  function remove_rh_session() {
+    const sessionsDir = './node_modules/rammerhead/sessions';
+      const sessionFilePath = path.join(sessionsDir, process.argv[3]);
+  
+      fs.unlink(sessionFilePath, (err) => {
+          if (err) {
+              console.error('Error deleting session:', err);
+              return;
+          }
+          console.log(`Session "${process.argv[3]}" deleted successfully.`);
+      });
+  }
+  
+
+async function version() {
+    try {
+      const packageJson = await readFile('./package.json', 'utf-8');
+      const { version } = JSON.parse(packageJson);
+      console.log('Version:', version);
+    } catch (error) {
+      console.error('Error reading package.json:', error);
+    }
+  }
 const parameter = process.argv[2];
 Handle(parameter);
