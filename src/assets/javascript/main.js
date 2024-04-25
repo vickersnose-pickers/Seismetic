0f68d0d332e3568f2625576639decef8f43e842f
rxfe-a <28RafaelA539@gmail.com>
2024-04-25 13:36:22 -0700

    CUSTOM PROXY IMPLEMENTATION

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -11,7 +11,7 @@ form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const url = search(address.value, JSON.parse(localStorage.getItem('basic-config')).search_engine || 'https://www.google.com/search?q=%s');
   var config = JSON.parse(localStorage.getItem('basic-config'));
-  const ProxyURL = 
+  const ProxyURL = GetProxyURL(url)
   eval(`${JSON.parse(localStorage.getItem('basic-config')).proxy_opening_mode || 'main'}('${ProxyURL}')`);
   window.parent.postMessage(`ReplaceURL('${url}')`, '*');
 });
@@ -40,4 +40,18 @@ function return_rh_url(url) {
 function return_nu_url(url) {
   const custom_url = `/webinstance/${url}`
   return custom_url;
+}
+
+function GetProxyURL(url) {
+  let DataSample = JSON.parse(localStorage.getItem('basic-config'));
+
+  if (DataSample.proxy == 1) {
+    return return_uv_url(url)
+  } else if (DataSample.proxy == 2) {
+    return return_rh_url(url)
+  } else if (DataSample.proxy == 3) {
+    return return_dn_url(url)
+  } else if (DataSample.proxy == 4) {
+    return return_nu_url(url)
+  }
 }
