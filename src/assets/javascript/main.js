307dc30af152c5e3b547e4629abd0228b9f11d20
rxfe-a <28RafaelA539@gmail.com>
2024-04-25 13:25:52 -0700

    Returns

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -11,11 +11,33 @@ form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const url = search(address.value, JSON.parse(localStorage.getItem('basic-config')).search_engine || 'https://www.google.com/search?q=%s');
   var config = JSON.parse(localStorage.getItem('basic-config'));
-  const ProxyURL = `${__uv$config.prefix}${__uv$config.encodeUrl(url)}`;
+  const ProxyURL = 
   eval(`${JSON.parse(localStorage.getItem('basic-config')).proxy_opening_mode || 'main'}('${ProxyURL}')`);
   window.parent.postMessage(`ReplaceURL('${url}')`, '*');
 });
 
 function main(url) {
   window.location.href = url;
+}
+
+//returns
+
+function return_uv_url(url) {
+  const custom_url = `${__uv$config.prefix}${__uv$config.encodeUrl(url)}`;
+  return custom_url
+}
+
+function return_dn_url(url) {
+  const custom_url = `${__dynamic$config.prefix}${__uv$config.encodeUrl(url)}`
+  return custom_url
+}
+
+function return_rh_url(url) {
+  const custom_url = `rammerhead.html#go=${url}`
+  return custom_url;
+}
+
+function return_nu_url(url) {
+  const custom_url = `/webinstance/${url}`
+  return custom_url;
 }
